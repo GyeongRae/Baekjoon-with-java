0c1c2d04c8c9143965ed936ab3462000cdd9da12
GyeongRae <67531670+GyeongRae@users.noreply.github.com>
2021-11-17 17:38:37 +0900

    Create 10872 팩토리얼.java

---
+++ 10000~11999/10872 팩토리얼.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        sc.close();
+        System.out.println(factorial(num));
+
+
+
+
+
+    }
+    public static int factorial(int n){
+        if(n < 1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+
+}
